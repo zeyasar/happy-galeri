ae6a9e6ccf7cb74f01b8a4ad0919bc27e44d942a
zeynep <zynpysr67@gmail.com>
2023-05-12 13:40:17 +0300

    fixed

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -1,20 +1,36 @@
-import React from 'react'
-import styles from './styles.module.css'
-import mainImage from '../../images/mainImage.svg'
+import React from "react";
+import styles from "./styles.module.css";
+import mainImage from "../../images/mainImage.svg";
 
 const Main = () => {
   return (
     <div className={styles.container}>
       <div className={styles.imageContainer}>
-      <img src={mainImage} alt='mainImage' style={{width: "100%", height: "auto"}}/>
-        
+        <img
+          src={mainImage}
+          alt="mainImage"
+          style={{ width: "100%", height: "auto" }}
+        />
       </div>
+      <div className={styles.imageTextContainer}>
+          <p className={styles.imageHeader}>Çocuklar İçin sanat eğitimi</p>
+          <p className={styles.imageText}>
+            Ücretsiz aile etkinliklerimizde bir sanat dünyası oyunu oynayın,
+            çocuklarınızla yaratın ve keşfedin
+          </p>
+        </div>
       <div className={styles.textContainer}>
         <p className={styles.header}>Happy Galeri’ye Hoşgeldiniz</p>
-        <p className={styles.paragraph}>bu muhteşem müzede keşfedilecek o kadar çok sanat eseri var ki! Bu yüzden en iyi şekilde yararlanmak için ziyaretinizi önceden planlayın. Temaya dayalı bir turu takip etmek için neden ziyaretçi parkurlarımızdan birini seçmiyorsunuz? Molaya ihtiyacınız olduğunda, müzenin dinlendirici bahçelerinden daha iyi neresi var?</p>
+        <p className={styles.paragraph}>
+          bu muhteşem müzede keşfedilecek o kadar çok sanat eseri var ki! Bu
+          yüzden en iyi şekilde yararlanmak için ziyaretinizi önceden planlayın.
+          Temaya dayalı bir turu takip etmek için neden ziyaretçi
+          parkurlarımızdan birini seçmiyorsunuz? Molaya ihtiyacınız olduğunda,
+          müzenin dinlendirici bahçelerinden daha iyi neresi var?
+        </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
